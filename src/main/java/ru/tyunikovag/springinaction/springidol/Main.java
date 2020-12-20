23875b9d8739e24d373167d03dc9cc213d100ae1
Tyunikov <tyunikovag@gmail.com>
2020-12-20 23:11:06 +0300

    start mvc branch

--- src/main/java/ru/tyunikovag/springinaction/springidol/Main.java
+++ src/main/java/ru/tyunikovag/springinaction/springidol/Main.java
@@ -3,14 +3,13 @@ package ru.tyunikovag.springinaction.springidol;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.tyunikovag.springinaction.springidol.exeptions.PerfomanceException;
-import ru.tyunikovag.springinaction.springidol.people.OneManBandArray;
 import ru.tyunikovag.springinaction.springidol.people.OneManBandMap;
 import ru.tyunikovag.springinaction.springidol.people.Performer;
 
 public class Main {
 
     public static void main(String[] args) throws PerfomanceException {
-        ApplicationContext context = new ClassPathXmlApplicationContext("app.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("springidol.xml");
         Performer performer = context.getBean("mapper", OneManBandMap.class);
         performer.perform();
 
